57f5aaf996095f51d4ad172430d9f4a0bd3831d5
Magnus Åkerfeldt <45148959+akerfel@users.noreply.github.com>
2025-06-29 11:10:54 +0200

    Time: 66 ms (92.57%), Space: 16.6 MB (46.98%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -12,8 +12,6 @@ class Solution(object):
         while L <= R:
             if people[L] + people[R] <= limit:
                 L += 1
-                R -= 1
-            else:
-                R -= 1
+            R -= 1
             boats += 1
         return boats
