37e4142626fc83c4076861d3f10f40399226d50d
Magnus Åkerfeldt <45148959+akerfel@users.noreply.github.com>
2025-06-30 23:57:24 +0200

    Time: 2473 ms (5.74%), Space: 20.7 MB (41.8%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero-sliding-window.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def minOperations(self, nums, x):
+        """
+        :type nums: List[int]
+        :type x: int
+        :rtype: int
+        """
+         # find widest window which sums exactly to target
+        target = sum(nums) - x
+
+        res = float("inf")
+        L = 0
+        cur = 0
+        for R in range(len(nums)):
+            cur += nums[R]
+            while cur > target and L <= R:
+                cur -= nums[L]
+                L += 1
+            if cur == target:
+                print(L, R)
+                width = R - L + 1
+                res = min(res, len(nums) - width)
+        
+        return -1 if res == float("inf") else res
