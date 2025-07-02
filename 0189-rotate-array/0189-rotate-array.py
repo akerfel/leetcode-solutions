97f38371f3e3e6317f57a9dbaafcd842cddd7576
Magnus Åkerfeldt <45148959+akerfel@users.noreply.github.com>
2025-07-02 20:31:24 +0200

    Time: 9 ms (35.88%), Space: 22.3 MB (94.23%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -5,6 +5,16 @@ class Solution(object):
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        copy = list(nums)
-        for i in range(len(nums)):
-            nums[i] = copy[(i - k) % len(nums)]
+        k = k % len(nums)
+        self.reverse(nums, 0, len(nums) - 1)
+        self.reverse(nums, 0, k - 1)
+        self.reverse(nums, k, len(nums) - 1)
+
+    # reverse the numbers from index L to R (inclusive) in the list
+    def reverse(self, nums, L, R):
+        while L < R:
+            tmp = nums[L]
+            nums[L] = nums[R]
+            nums[R] = tmp
+            L += 1
+            R -= 1
