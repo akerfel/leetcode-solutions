a45a7ee880770b3b119fc30f16b848f0959000b6
Magnus Åkerfeldt <45148959+akerfel@users.noreply.github.com>
2025-07-05 12:13:34 +0200

    Time: 16 ms (61.43%), Space: 23.7 MB (60.45%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.py
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.py
@@ -4,17 +4,19 @@ import heapq
 # param_1 = obj.add(val)
 class KthLargest:
 
+    # Time complexity: O(n)
     # create min heap of size k
     def __init__(self, k: int, nums: List[int]):
         self.heap = nums
         self.k = k
-        heapq.heapify(self.heap) # O(n) time
+        heapq.heapify(self.heap) # O(n)
         while len(self.heap) > self.k:
             heapq.heappop(self.heap)
 
+    # Time complexity: O(log n)
     # heappush to heap. If more than k elements: pop top
     def add(self, val: int) -> int:
-        heapq.heappush(self.heap, val)
+        heapq.heappush(self.heap, val) # O(log n)
         if len(self.heap) > self.k:
             heapq.heappop(self.heap)
         return self.heap[0]
