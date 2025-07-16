c2b4dd81aea81f9ca37ef177a1ee31918e7e5ce3
Magnus Åkerfeldt <45148959+akerfel@users.noreply.github.com>
2025-07-16 15:26:14 +0200

    Time: 187 ms (58.47%), Space: 40.4 MB (49.86%) - LeetHub

---
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.py
@@ -0,0 +1,20 @@
+import heapq
+
+class Solution:
+    def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        tuples = [(n1, n2) for n1, n2 in zip(nums1, nums2)]
+        tuples.sort(key=lambda tup: tup[1], reverse=True) # sort based on nums2
+
+        res = 0
+        n1heap = list() # currently chosen values in nums1
+        n1sum = 0
+
+        # n2 is always the current min value of chosen values in nums2, because of the sorting
+        for n1, n2 in tuples:
+            heapq.heappush(n1heap, n1)
+            n1sum += n1
+            if len(n1heap) > k:
+                n1sum -= heapq.heappop(n1heap)
+            if len(n1heap) == k:
+                res = max(res, n1sum * n2)
+        return res
