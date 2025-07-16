210506553674875e1cbba0e09ea5824b2ef0380b
Magnus Åkerfeldt <45148959+akerfel@users.noreply.github.com>
2025-07-16 15:27:13 +0200

    Time: 171 ms (81.36%), Space: 40 MB (86.48%) - LeetHub

--- 2542-maximum-subsequence-score/2542-maximum-subsequence-score.py
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.py
@@ -2,7 +2,7 @@ import heapq
 
 class Solution:
     def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
-        tuples = [(n1, n2) for n1, n2 in zip(nums1, nums2)]
+        tuples = list(zip(nums1, nums2))
         tuples.sort(key=lambda tup: tup[1], reverse=True) # sort based on nums2
 
         res = 0
